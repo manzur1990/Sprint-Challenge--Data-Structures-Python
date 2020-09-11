1e651e3d89b4186d013e56104df1a4014455531b
JM <manzur-1990@hotmail.com>
2020-09-11 11:31:37 -0500

    buffer ring

--- names/names.py
+++ names/names.py
@@ -10,6 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
@@ -18,9 +19,10 @@ for name_1 in names_1:
         if name_1 == name_2:
             duplicates.append(name_1)
 
+
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.next_oldest_value = 0
+        self.items = [None for item in range(capacity)]
 
     def append(self, item):
-        pass
+
+        self.items[self.next_oldest_value] = item
+
+        self.next_oldest_value += 1
+        if self.next_oldest_value > (self.capacity - 1):
+            self.next_oldest_value = 0
 
     def get(self):
-        pass
+        return [item for item in self.items if item is not None]
